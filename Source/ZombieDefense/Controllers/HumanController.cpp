a165af1d05d8813b52d17c1307c0c18bdd48b834
Sergey Firsov <fearthunder@yandex.ru>
2020-06-17 12:43:14 +0300

    Add human controller

---
+++ Source/ZombieDefense/Controllers/HumanController.cpp
@@ -0,0 +1,5 @@
+// Copyright 2020 Sergey Firsov
+
+
+#include "HumanController.h"
+
---
+++ Source/ZombieDefense/Controllers/HumanController.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HumanController.generated.h"
+
+
+UCLASS()
+class ZOMBIEDEFENSE_API AHumanController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
