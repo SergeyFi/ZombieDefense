3019986fef8b88aaddc256b209a61df746168333
Sergey Firsov <fearthunder@yandex.ru>
2020-07-20 22:00:31 +0300

    Add ammo component

---
+++ Source/ZombieDefense/Components/AmmoComponent.cpp
@@ -0,0 +1,115 @@
+// Copyright 2020 Sergey Firsov
+
+
+#include "AmmoComponent.h"
+#include "FireComponent.h"
+
+UAmmoComponent::UAmmoComponent()
+{
+    ClipSizeInitial = 1;
+    ClipSizeCurrent = ClipSizeInitial;
+    ReloadTime = 1.0f;
+    bInfinityAmmo = true;
+}
+
+void UAmmoComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    BindToFireComponent();
+
+}
+
+void UAmmoComponent::BindToFireComponent()
+{
+    TArray<UFireComponent*> FireComponents;
+    GetOwner()->GetComponents<UFireComponent>(FireComponents);
+
+    for (auto FireComponent : FireComponents)
+    {
+        if (FireComponent)
+        {
+            FireComponent->OnProjectileSpawned.AddDynamic(this, &UAmmoComponent::OnProjectileSpawned);
+        }
+    }
+}
+
+void UAmmoComponent::OnProjectileSpawned(AActor* Projectile)
+{
+    RemoveAmmoClip(1);
+}
+
+void UAmmoComponent::RemoveAmmoClip(int AmmoRemoved)
+{
+    ClipSizeCurrent -= AmmoRemoved;
+
+    if (ClipSizeCurrent <= 0)
+    {
+        ClipSizeCurrent = 0;
+
+        BlockFire();
+        
+        Reload();
+    }
+}
+
+void UAmmoComponent::BlockFire()
+{
+    TArray<UFireComponent*> FireComponents;
+    GetOwner()->GetComponents<UFireComponent>(FireComponents);
+
+    for (auto FireComponent : FireComponents)
+    {
+        if (FireComponent)
+        {
+            FireComponent->BlockFire();
+        }
+    }
+}
+
+void UAmmoComponent::Reload()
+{
+    ClipSizeCurrent = ClipSizeInitial;
+
+    RemoveAmmo(ClipSizeInitial);
+    
+    GetWorld()->GetTimerManager().SetTimer(ReloadTimer, this, &UAmmoComponent::UnblockFire,
+        ReloadTime, false, ReloadTime);
+}
+
+void UAmmoComponent::UnblockFire()
+{
+    TArray<UFireComponent*> FireComponents;
+    GetOwner()->GetComponents<UFireComponent>(FireComponents);
+
+    for (auto FireComponent : FireComponents)
+    {
+        if (FireComponent)
+        {
+            FireComponent->UnblockFire();
+        }
+    }
+}
+
+void UAmmoComponent::AddAmmo(int AmmoIncrease)
+{
+    Ammo += AmmoIncrease;
+
+    if (Ammo > 0)
+    {
+        UnblockFire();
+    }
+}
+
+void UAmmoComponent::RemoveAmmo(int AmmoRemoved)
+{
+    if (!bInfinityAmmo)
+    {
+        Ammo -= AmmoRemoved;
+
+        if (Ammo <= 0)
+        {
+            Ammo = 0;
+        }
+    }
+}
---
+++ Source/ZombieDefense/Components/AmmoComponent.h
@@ -0,0 +1,57 @@
+// Copyright 2020 Sergey Firsov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AmmoComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ZOMBIEDEFENSE_API UAmmoComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+protected:
+	
+	void BindToFireComponent();
+
+	UFUNCTION()
+	void OnProjectileSpawned(AActor* Projectile);
+
+	void BlockFire();
+
+	void UnblockFire();
+
+	UPROPERTY(EditAnywhere)
+	int Ammo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int ClipSizeInitial;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int ClipSizeCurrent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float ReloadTime;
+
+	UPROPERTY(EditAnywhere)
+	bool bInfinityAmmo;
+
+	void RemoveAmmoClip(int AmmoRemoved);
+
+	virtual void BeginPlay() override;
+
+	void Reload();
+
+	FTimerHandle ReloadTimer;
+
+public:
+
+	UAmmoComponent();
+	
+	void AddAmmo(int AmmoIncrease);
+
+	void RemoveAmmo(int AmmoRemoved);
+	
+};
